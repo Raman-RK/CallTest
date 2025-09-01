2850c4612f4fc33a6378692cb4cee8af1a3e224c
Raman-RK <rtester341@gmail.com>
2025-09-01 19:30:41 +0530

    locators

---
+++ login/main_menu_locators.py
@@ -0,0 +1,2 @@
+admin_settings = '//div[@class="flex flex-col items-center justify-center gap-1"]//small[text()="Admin"]'  #use for all changing only text
+user_sub_tab = '//div[@class="flex items-center w-full px-3 h-12 gap-2 cursor-pointer font-medium " and text()="Users"]'
